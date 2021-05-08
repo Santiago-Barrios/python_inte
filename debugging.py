81956f66ad08729ec2a5138972758a5314fb5817
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2021-05-07 19:07:28 -0500

    capturando errores

--- debugging.py
+++ debugging.py
@@ -1,15 +1,23 @@
 def divisors(num):
     divisors = []
     for i in range (1, num +1):
-        if num % i == 1:
+        if num % i == 0:
             divisors.append(i)
     return divisors
 
 
 def run():
-    num = int(input('ingresa un numero: '))
-    print(divisors(num))
-    print('termino mi programa')
+    try:
+        num = int(input('ingresa un numero: '))
+        if num <=0:
+            raise ValueError( 'El valor debe ser positivo' )
+        elif type(num) == str:
+            raise ValueError( 'El valor debe ser un número' )
+        print(divisors(num))
+        print('termino mi programa')
+    except ValueError as ve:
+            print(ve)
+
 
 if __name__ == '__main__':
     run()
